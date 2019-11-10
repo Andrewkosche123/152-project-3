ec9725aa205f12a807042b2d1dd04a6761996735
Ali Zand <ali.zand@realpage.com>
2019-11-10 15:19:41 -0800

    Call visitor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,22 @@ import org.antlr.v4.runtime.ListTokenSource;
 public class Main {
     public static void main(String[] args) {
         try {
-            MainLexer ml = new MainLexer(CharStreams.fromFileName("input"));
-            ListTokenSource lts = new ListTokenSource(ml.getAllTokens());
-            BufferedTokenStream bts = new BufferedTokenStream(lts);
-            MainParser mp = new MainParser(bts);
+            MainLexer mll = new MainLexer(CharStreams.fromFileName("input"));
+            ListTokenSource ltsl = new ListTokenSource(mll.getAllTokens());
+            BufferedTokenStream btsl = new BufferedTokenStream(ltsl);
+            MainParser mp = new MainParser(btsl);
+
+            System.out.println("-----===== PARSE USING LISTENER =====-----");
             mp.addParseListener(new MainBaseListener());
             mp.program();
+
+            System.out.println("\n\n\n\n-----===== PARSE USING VISITOR =====-----");
+            MainLexer mlv = new MainLexer(CharStreams.fromFileName("input"));
+            ListTokenSource ltsv = new ListTokenSource(mlv.getAllTokens());
+            BufferedTokenStream btsv = new BufferedTokenStream(ltsv);
+            MainParser mpv = new MainParser(btsv);
+            MyVisitor v = new MyVisitor();
+            v.visitProgram(mpv.program());
         } catch (Exception e) {
             e.printStackTrace();
         }
